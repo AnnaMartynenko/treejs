6f12b3dac1d3f4394b16880fde06c4ada0bf7ee9
Anna Martynenko <anna_martynenko@epam.com>
2021-09-01 23:44:57 +0300

    my changes

--- src/script.js
+++ src/script.js
@@ -2,104 +2,227 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'dat.gui'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
-// Debug
-const gui = new dat.GUI()
+const TRAY = document.getElementById('js-tray-slide');
 
-// Canvas
-const canvas = document.querySelector('canvas.webgl')
+var theModel;
 
-// Scene
-const scene = new THREE.Scene()
+const MODEL_PATH = "simple_sedan_scaled.glb";
 
-// Objects
-const geometry = new THREE.TorusGeometry( .7, .2, 16, 100 );
+const colors = [
+{
+    color: '66533C'
+},
+{
+    color: '173A2F'
+},
+{
+    color: '153944'
+},
+{
+    color: '27548D'
+},
+{
+    color: '438AAC'
+}  
+]
 
-// Materials
+const BACKGROUND_COLOR = 0xf1f1f1;
+// Init the scene
+const scene = new THREE.Scene();
+// Set background
+scene.background = new THREE.Color(BACKGROUND_COLOR );
+scene.fog = new THREE.Fog(BACKGROUND_COLOR, 20, 100);
 
-const material = new THREE.MeshBasicMaterial()
-material.color = new THREE.Color(0xff0000)
+const canvas = document.querySelector('#c');
 
-// Mesh
-const sphere = new THREE.Mesh(geometry,material)
-scene.add(sphere)
+// Init the renderer
+const renderer = new THREE.WebGLRenderer({canvas, antialias: true});
 
-// Lights
+renderer.shadowMap.enabled = true;
+renderer.setPixelRatio(window.devicePixelRatio); 
 
-const pointLight = new THREE.PointLight(0xffffff, 0.1)
-pointLight.position.x = 2
-pointLight.position.y = 3
-pointLight.position.z = 4
-scene.add(pointLight)
+var cameraFar = 5;
 
-/**
- * Sizes
- */
-const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
+document.body.appendChild(renderer.domElement);
+
+// Add a camerra
+var camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = cameraFar;
+camera.position.x = 0;
+
+// Initial material
+const INITIAL_MTL = new THREE.MeshStandardMaterial( { color: 0xf1f1f1, shininess: 10 } );
+//const INITIAL_MTL = new THREE.MeshPhongMaterial( { color: 0xf1f1f1, shininess: 10 } );
+
+const INITIAL_MAP = [
+  {childID: "back wheel", mtl: INITIAL_MTL},
+  {childID: "front wheel", mtl: INITIAL_MTL},
+  {childID: "front-glass", mtl: INITIAL_MTL},
+  {childID: "side-glass", mtl: INITIAL_MTL},
+  {childID: "left-mirror", mtl: INITIAL_MTL},  
+  {childID: "korpus-right", mtl: INITIAL_MTL},
+  {childID: "korpus", mtl: INITIAL_MTL},  
+  {childID: "ruchka-front-left", mtl: INITIAL_MTL},
+  {childID: "ruchka-zadnya-liva", mtl: INITIAL_MTL},
+];
+
+// Init the object loader
+var loader = new GLTFLoader();
+
+loader.load(MODEL_PATH, function(gltf) {
+  theModel = gltf.scene;
+
+    theModel.traverse((o) => {
+     if (o.isMesh) {
+       o.castShadow = true;
+       o.receiveShadow = true;
+     }
+   });
+  
+// Set the models initial scale   
+  theModel.scale.set(2,2,2);
+  theModel.rotation.y = Math.PI;
+
+  // Offset the y position a bit
+  //theModel.position.y = -1;
+  
+  theModel.position.y = -0.65;
+
+  // Set initial textures
+  for (let object of INITIAL_MAP) {
+    initColor(theModel, object.childID, object.mtl);
+  }
+
+  // Add the model to the scene
+  scene.add(theModel);
+
+}, undefined, function(error) {
+  console.error(error)
+});
+
+// Function - Add the textures to the models
+function initColor(parent, type, mtl) {
+  parent.traverse((o) => {
+   if (o.isMesh) {
+     if (o.name.includes(type)) {
+          o.material = mtl;
+          o.nameID = type; // Set a new property to identify this object
+       }
+   }
+ });
 }
 
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+// Add lights
+var hemiLight = new THREE.HemisphereLight( 0xffffff, 0xffffff, 0.61 );
+    hemiLight.position.set( 0, 50, 0 );
+// Add hemisphere light to scene   
+scene.add( hemiLight );
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+var dirLight = new THREE.DirectionalLight( 0xffffff, 0.54 );
+    dirLight.position.set( -8, 12, 8 );
+    dirLight.castShadow = true;
+    dirLight.shadow.mapSize = new THREE.Vector2(1024, 1024);
+// Add directional Light to scene    
+    scene.add( dirLight );
 
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-})
+// Floor
+var floorGeometry = new THREE.PlaneGeometry(5000, 5000, 1, 1);
+var floorMaterial = new THREE.MeshPhongMaterial({
+  color: 0xeeeeee,
+  shininess: 0
+});
 
-/**
- * Camera
- */
-// Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.x = 0
-camera.position.y = 0
-camera.position.z = 2
-scene.add(camera)
+var floor = new THREE.Mesh(floorGeometry, floorMaterial);
+floor.rotation.x = -0.5 * Math.PI;
+floor.receiveShadow = true;
+floor.position.y = -1;
+scene.add(floor);
 
-// Controls
-// const controls = new OrbitControls(camera, canvas)
-// controls.enableDamping = true
+// Add controls
+var controls = new OrbitControls( camera, renderer.domElement );
+controls.maxPolarAngle = Math.PI / 2;
+controls.minPolarAngle = Math.PI / 3;
+controls.enableDamping = true;
+controls.enablePan = false;
+controls.dampingFactor = 0.1;
+controls.autoRotate = false; // Toggle this if you'd like the chair to automatically rotate
+controls.autoRotateSpeed = 0.2; // 30
 
-/**
- * Renderer
- */
-const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-})
-renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-
-/**
- * Animate
- */
-
-const clock = new THREE.Clock()
-
-const tick = () =>
-{
-
-    const elapsedTime = clock.getElapsedTime()
-
-    // Update objects
-    sphere.rotation.y = .5 * elapsedTime
-
-    // Update Orbital Controls
-    // controls.update()
-
-    // Render
-    renderer.render(scene, camera)
-
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
+function animate() {
+  renderer.render(scene, camera);
+  requestAnimationFrame(animate);
+  
+  if (resizeRendererToDisplaySize(renderer)) {
+    const canvas = renderer.domElement;
+    camera.aspect = canvas.clientWidth / canvas.clientHeight;
+    camera.updateProjectionMatrix();
+  }
 }
 
-tick()
+animate();
+
+// Function - New resizing method
+function resizeRendererToDisplaySize(renderer) {
+  const canvas = renderer.domElement;
+  var width = window.innerWidth;
+  var height = window.innerHeight;
+  var canvasPixelWidth = canvas.width / window.devicePixelRatio;
+  var canvasPixelHeight = canvas.height / window.devicePixelRatio;
+
+  const needResize = canvasPixelWidth !== width || canvasPixelHeight !== height;
+  if (needResize) {
+    
+    renderer.setSize(width, height, false);
+  }
+  return needResize;
+}
+
+// Function - Build Colors
+
+function buildColors(colors) {
+  for (let [i, color] of colors.entries()) {
+    let swatch = document.createElement('div');
+    swatch.classList.add('tray__swatch');
+
+      swatch.style.background = "#" + color.color;
+
+    swatch.setAttribute('data-key', i);
+    TRAY.append(swatch);
+  }
+}
+
+buildColors(colors);
+
+// Swatches
+const swatches = document.querySelectorAll(".tray__swatch");
+
+for (const swatch of swatches) {
+  swatch.addEventListener('click', selectSwatch);
+}
+
+function selectSwatch(e) {
+     let color = colors[parseInt(e.target.dataset.key)];
+     let new_mtl;
+
+      new_mtl = new THREE.MeshStandardMaterial({ //THREE.MeshPhongMaterial // MeshBasicMaterial
+          color: parseInt('0x' + color.color),
+          shininess: color.shininess ? color.shininess : 10
+          
+        });
+    
+    //setMaterial(theModel, 'korpus-right', new_mtl);//front wheel//left-mirror
+    setMaterial(theModel, 'ruchka-zadnya-liva', new_mtl);
+    setMaterial(theModel, 'ruchka-front-left', new_mtl);
+}
+
+function setMaterial(parent, type, mtl) {
+  parent.traverse((o) => {
+   if (o.isMesh && o.nameID != null) {
+     if (o.nameID == type) {
+          o.material = mtl;
+       }
+   }
+ });
+}
